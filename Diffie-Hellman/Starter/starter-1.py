d690117d4d07a90a0de6e8caff4c4873927c6709
keandk <69447494+keandk@users.noreply.github.com>
2023-05-03 01:16:14 +0700

    Revised and corrected

--- Diffie-Hellman/Starter/starter-1.py
+++ Diffie-Hellman/Starter/starter-1.py
@@ -1,22 +1,3 @@
-def extended_euclidean_algorithm(a, b):
-    r0, r1 = a, b
-    t0, t1 = 0, 1
-
-    while r1 != 0:
-        q = r0 // r1
-        r0, r1 = r1, r0 % r1
-        t0, t1 = t1, t0 - q * t1
-
-    return r0, t0
-
-def modular_inverse(g, p):
-    gcd, t = extended_euclidean_algorithm(g, p)
-
-    if gcd != 1:
-        raise ValueError("g and p are not coprime")
-
-    return t % p
-
 p = 991
 g = 209
 
